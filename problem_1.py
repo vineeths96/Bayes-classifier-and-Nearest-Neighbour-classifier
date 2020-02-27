ba03f94a21faabfb6349d420e1024a92594ca771
vineeths96 <vinsree1996@gmail.com>
2020-02-27 09:10:56 +0530

    Final draft

--- problem_1.py
+++ problem_1.py
@@ -3,11 +3,11 @@ from problem_1.problem_1a import problem_1a
 from problem_1.problem_1b import problem_1b
 from problem_1.problem_1c import problem_1c
 
-"""X_train, Y_train, X_test, Y_test = load_data('a')
+X_train, Y_train, X_test, Y_test = load_data('a')
 problem_1a(X_train, Y_train, X_test, Y_test)
 
 X_train, Y_train, X_test, Y_test = load_data('b')
-problem_1b(X_train, Y_train, X_test, Y_test)"""
+problem_1b(X_train, Y_train, X_test, Y_test)
 
 X_train, Y_train, X_test, Y_test = load_data('c')
 problem_1c(X_train, Y_train, X_test, Y_test)
