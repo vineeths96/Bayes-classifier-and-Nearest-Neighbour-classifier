299c68de357bfe34517508e89c8ae00c545619bf
vineeths96 <vinsree1996@gmail.com>
2020-02-28 10:28:22 +0530

    Final draft

--- problem_1/bayes_gaussian_exponential.py
+++ problem_1/bayes_gaussian_exponential.py
@@ -13,8 +13,9 @@ def bayes_gaussian_exponential(theta_0, theta_1, X_test, p0=0.5, p1=0.5):
     Y_pred = np.zeros(len(X_test))
 
     for idx in range(len(X_test)):
-        X = np.abs(X_test[idx])
+        X = X_test[idx]
         q0 = 1/(2*np.pi*(det_sigma_0**0.5)) * np.exp(-0.5 * (X-mu_0)@ inv_sigma_0 @np.transpose(X-mu_0)) *p0
+        X = np.abs(X)
         q1 = lambda_1 * np.exp(-lambda_1 * X[0]) * lambda_2 * np.exp(-lambda_2 * X[1]) *p1
 
         if q0 >= q1:
